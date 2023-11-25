dfd79e8cdcd7017e3ddc8c93953f3fef6d5f6cbb
Takanori Oishi <takanori0014@live.jp>
2023-11-25 22:15:10 +0900

    feat: Add npm profile

--- src/card.ts
+++ src/card.ts
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+import * as os from "os";
+import { bicstone } from "./index.js";
 
-console.log("test");
+const output = bicstone.name;
+
+process.stdout.write(output + os.EOL);
+process.exitCode = 0;
